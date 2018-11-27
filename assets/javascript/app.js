8b2b981d263620060a93ddeea73f98a503011f4c
Jenae <Jenaenay15@yahoo.com>
2018-11-27 13:32:35 -0700

    made folders

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+//create a game that is capable of asking questions, generating right and wrong answers, calculating points,
+//and working in a time frame.
+//create a theme and research questions
+//create the HTML and CSS to match the theme.
+//what code needs to be written to generate questions
+//what code needs to be written to receive the answers
+//calculate score
+//reset once the timer is complete
+//alert the player once the game is done
